7b524bebcf0d8d149f4021a84cc74c2f2b0745fc
strick <strick@yak.net>
2021-09-24 07:27:57 -0700

    slot structs String and Slice.

--- runtime_c.h
+++ runtime_c.h
@@ -7,6 +7,18 @@ typedef signed char t_int1;
 typedef unsigned int t_uint2;
 typedef signed int t_int2;
 
+struct String {
+    char* base;
+    int offset;
+    int len;
+};
+
+struct Slice {
+    char* base;
+    int offset;
+    int len;
+};
+
 void F_BUILTIN_println(int i);
 
 #endif
