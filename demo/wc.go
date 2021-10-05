cdb3c8a33198119cb5461c4e96e6f1ba1c4c52e1
strick <strick@yak.net>
2021-10-04 20:17:10 -0700

    demo/wc.go

---
+++ demo/wc.go
@@ -0,0 +1,46 @@
+// +build foo
+
+package main
+
+import "io"
+import "os"
+import "log"
+
+func main() {
+	onebyte := make([]byte, 1)
+	for {
+		n, err := os.Stdin.Read(onebyte)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("cannot read stdin: %v", err)
+		}
+		if n != 1 {
+			log.Fatalf("expected to read 1 byte, got %d", n)
+		}
+		Count(onebyte[0])
+	}
+	println(Lines, Words, Bytes)
+}
+
+var Bytes int
+var Words int
+var Lines int
+var wasWhite = true
+
+func Count(ch byte) {
+	Bytes++
+	switch ch {
+	case '\n', '\r':
+		Lines++
+		wasWhite = true
+	case ' ', '\t':
+		wasWhite = true
+	default:
+		if wasWhite {
+			Words++
+		}
+		wasWhite = false
+	}
+}
