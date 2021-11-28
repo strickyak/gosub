e730d4f1ab03030a9372fc8dcc4ab0c202882967
strick <strick@yak.net>
2021-11-27 23:03:56 -0800

    Log options in gosub.go main()

--- gosub.go
+++ gosub.go
@@ -11,6 +11,8 @@ import (
 var LibDir = flag.String("libdir", "lib", "where to import libs from")
 
 func main() {
+	log.SetFlags(0)
+	log.SetPrefix("## ")
 	flag.Parse()
 	CompileToC(os.Stdin, "stdin", os.Stdout, &Options{
 		LibDir: *LibDir,
