d1a88edaebe62fe189335ca27b49814e9203d70c
strick <strick@yak.net>
2021-12-20 22:53:41 -0800

    Test2, Test3

--- parser/parse_test.go
+++ parser/parse_test.go
@@ -20,7 +20,7 @@ func Simplify(s string) string {
 
 func SimplyEqual(t *testing.T, a, b string) {
 	t.Logf("SimplyEqual: a = [[[[[%s]]]]]", a)
-	t.Logf("SimplyEqual: b = (((((%s)))))", b)
+	t.Logf("SimplyEqual: b = [[[[[%s]]]]]", b)
 	sa := Simplify(a)
 	sb := Simplify(b)
 	t.Logf("SimplyEqual: sa = %q", sa)
@@ -34,14 +34,34 @@ func Test1(t *testing.T) {
 	prog := `var foo int`
 	r := bytes.NewBufferString(prog)
 	w := bytes.NewBufferString("")
-	//var w bytes.Buffer
 	CompileToC(r, "Test1", w, &Options{
 		LibDir:      "/none/",
 		SkipBuiltin: true,
 	})
-	//fmt.Printf("[[[[[\n%s\n]]]]]\n", w.String())
-	//fmt.Printf("[[[[[\n%s\n]]]]]\n", Simplify(w.String()))
-	t.Logf("Test1: => %q", w.String())
 	want := `P_int main__foo; void INIT() {}`
+	SimplyEqual(t, w.String(), want)
+}
+
+func Test2(t *testing.T) {
+	prog := `type Apple struct { Worm int }`
+	r := bytes.NewBufferString(prog)
+	w := bytes.NewBufferString("")
+	CompileToC(r, "Test1", w, &Options{
+		LibDir:      "/none/",
+		SkipBuiltin: true,
+	})
+	want := `typedef Struct main__Apple; void INIT(){}`
+	SimplyEqual(t, w.String(), want)
+}
+
+func Test3(t *testing.T) {
+	prog := `type Frobber interface { Frob()string }`
+	r := bytes.NewBufferString(prog)
+	w := bytes.NewBufferString("")
+	CompileToC(r, "Test1", w, &Options{
+		LibDir:      "/none/",
+		SkipBuiltin: true,
+	})
+	want := `typedef Interface main__Frobber; voidINIT(){}`
 	SimplyEqual(t, w.String(), want)
 }
